ea867da583713a0f7f462441091eaa1140cd392f
Firmant Xu <32982120+demogest@users.noreply.github.com>
2021-05-28 00:07:46 +0800

    Update 5-1.cpp

--- 5-1.cpp
+++ 5-1.cpp
@@ -38,6 +38,7 @@ private:
     vector<bool> visited;
 
 public:
+    int getedgn();
     void CreateFromMatrix(vector<char> &vex,vector<vector<int> > &a, int n, int m);
     void CreateGraph();
     void PrintGraph();
@@ -45,12 +46,22 @@ public:
     void BFS();
     int find(int x);
     int Kruskal();
+    void clr(); 
 };
-
+int ALGraph::getedgn(){
+    return this->edgNum;
+}
+void ALGraph::clr(){
+    this->parent.clear();
+    this->adjList.clear();
+    this->visited.clear();
+    this->edge.clear();
+}
 void ALGraph::CreateFromMatrix(vector<char> &vex,vector<vector<int> > &a, int n, int m){
     int cnt=0;
     this->edgNum = m;
     this->vexNum = n;
+    if (this->vexNum == 0 || this->edgNum == 0) return;
     this->parent.resize(this->vexNum);
     this->adjList.resize(this->vexNum);
     this->visited.resize(this->vexNum);
@@ -90,13 +101,15 @@ void ALGraph::CreateGraph()
     this->visited.resize(this->vexNum);
     this->parent.resize(this->vexNum);
     cin >> this->edgNum;
-    this->edge.resize(this->edgNum);
+    if(this->edgNum>0)
+        this->edge.resize(this->edgNum);
     cout<<"Input the info of vertex:\n";
     for (int i = 0; i<this->vexNum; i++){
         cin>>this->adjList[i].vertex;
         this->adjList[i].firstarc = NULL;
     }
-    cout << "Input " << this->edgNum << " edge's info:\n";
+    if (this->edgNum>0)
+        cout << "Input " << this->edgNum << " edge's info:\n";
     int cnt=0;
     for (int i = 0; i<this->edgNum; i++){
         int src, dest, weight;
@@ -124,13 +137,13 @@ void ALGraph::BFS()
         this->visited[i] = false;
     queue<int> q;
     cout<<this->adjList[0].vertex<<" ";
-    this->visited[this->adjList[0].vertex]=true;
-    q.push(this->adjList[0].vertex);
+    this->visited[0]=true;
+    q.push(0);
     while(!q.empty()){
         for (ENode *e = this->adjList[q.front()].firstarc;e!=NULL;e=e->next){
             if (!this->visited[e->adjvex]){
                 this->visited[e->adjvex] = true;
-                cout<<e->adjvex<<" ";
+                cout<<this->adjList[e->adjvex].vertex<<" ";
                 q.push(e->adjvex);
             }
         }
@@ -192,6 +205,7 @@ void CreateByMatrix(vector<char> &vex, vector<vector<int> > &matrix,int &vexNum,
         matrix[src][dest] = weight;
         matrix[dest][src] = weight;
     }
+    cout<<"result:\n";
     for (int i=0;i<matrix.size();i++){
         for (int j=0;j<matrix[i].size();j++)
             cout<<matrix[i][j]<<" ";
@@ -206,7 +220,7 @@ int main(){
     while(true){
         cout<<"1.Store by AdjMatrix\n";
         cout<<"2.Store by AdjList(output the result of bfs and dfs)\n";
-        cout<<"3.Urban transportation network planning（Kruskal)\n";
+        cout<<"3.Urban transportation network planning(Kruskal)\n";
         cin>>k;
         switch (k)
         {
@@ -218,19 +232,31 @@ int main(){
                 vex.resize(vexNum);
                 CreateByMatrix(vex,ma,vexNum, edgNum);
                 list->CreateFromMatrix(vex,ma,vexNum,edgNum);
-                break;
+	            ma.clear();
+                vex.clear();
+	            break;
             case 2:
                 int root;
                 list->CreateGraph();
-                list->BFS();
-                cout<<"Input the number which search begin:\n";
-                cin>>root;
-                list->DFS(root);
-                cout<<endl;
-                break;
+	            if (list->getedgn()>0){                
+	                cout<<"BFS:\n";
+	                list->BFS();
+                    //cout<<"Input the number which search begin:\n";
+                    //cin>>root;
+                    cout<<"DFS:\n";
+	                list->DFS(0);
+                    cout<<endl;
+                }
+	            else
+	                cout<<"Empty\n";
+	            list->clr();
+	            break;
             case 3:
+	            list->CreateGraph();
+            	cout<<"Result:\n";
                 list->Kruskal();
                 cout<<endl;
+	            list->clr();
                 break;
             case 0:
                 return 0;
